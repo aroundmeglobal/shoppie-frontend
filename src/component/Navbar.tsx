7f70e98021c0951eafd84f6a8f0de12388730169
PrayagN <prayag.n.manojnair@gmail.com>
2025-03-19 15:50:05 +0530

    removed logout from navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -42,12 +42,12 @@ function Navbar() {
       </Link>
       {pathname === "/" && (
         <div className="flex gap-5 ">
-          <button
+          {/* <button
             onClick={onLogout}
             className="cursor-pointer font-bold text-[10px] md:text-[16px]"
           >
             {isLoggedIn ? "Log out" : ""}
-          </button>
+          </button> */}
           {/* <a
             onClick={onLogin}
             className="cursor-pointer relative  flex items-center  justify-center px-2 py-[2px] text-white text-[15px]  leading-[25px] tracking-[-0.5px] bg-[#0055FF] rounded-[10px] border-[2.1px] border-white/15 shadow-[0px_8px_40px_rgba(0,85,255,0.5),0px_0px_10px_rgba(255,255,255,0)_inset,0px_0px_0px_1px_rgba(0,85,255,0.12)] transition-all duration-300 hover:shadow-[0px_8px_50px_rgba(0,85,255,0.6),0px_0px_12px_rgba(255,255,255,0)_inset,0px_0px_0px_1px_rgba(0,85,255,0.15)]"
