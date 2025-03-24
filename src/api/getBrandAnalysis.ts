d56475da09da9a2d6bec599b53eacef66eefb15b
PrayagN <prayag.n.manojnair@gmail.com>
2025-03-24 12:26:00 +0530

    updated brandAnalysis

--- src/api/getBrandAnalysis.ts
+++ src/api/getBrandAnalysis.ts
@@ -1,40 +1,57 @@
+import api from "@/lib/axiosInstance";
+
 export async function fetchUniqueViewData(brandId: string) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/unique-view?brand_id=${brandId}`
-    );
-    if (!response.ok) {
-      throw new Error("Error fetching unique view data");
+  console.log(brandId);
+
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/unique-view?brand_id=${brandId}`
+  );
+
+  return response.data;
+}
+
+export async function fetchTotalSentMessagesData(brandId: string) {
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-sent-messages?brand_id=${brandId}`
+  );
+  return response.data;
+}
+
+export async function fetchTotalFirstMessagesData(brandId: string) {
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-first-messages?brand_id=${brandId}`
+  );
+  return response.data;
+}
+
+export async function fetchTotalWidgetTapsData(brandId: string) {
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-widget-taps?brand_id=${brandId}`
+  );
+  return response.data;
+}
+
+export async function getAverage(brandId: string) {
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/average-messages-per-conversation/?brand_id=${brandId}`
+  );
+  return response.data;
+}
+
+export async function getProductTaps(brandId: string) {
+  const response = await api.get(
+    `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/product-taps/?brand_id=${brandId}`
+  );
+  return response.data;
+}
+
+export async function getProducts(brandId: string, product_title: any) {
+  const products = Object.keys(product_title);
+  const response = await api.post(
+    `${process.env.NEXT_PUBLIC_DEVBASEURL}/files/product-name-details?brand_id=${brandId}`,
+    {
+      products: products,
     }
-    return response.json();
-  }
-  
-  export async function fetchTotalSentMessagesData(brandId: string) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-sent-messages?brand_id=${brandId}`
-    );
-    if (!response.ok) {
-      throw new Error("Error fetching total sent messages data");
-    }
-    return response.json();
-  }
-  
-  export async function fetchTotalFirstMessagesData(brandId: string) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-first-messages?brand_id=${brandId}`
-    );
-    if (!response.ok) {
-      throw new Error("Error fetching total first messages data");
-    }
-    return response.json();
-  }
-  
-  export async function fetchTotalWidgetTapsData(brandId: string) {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_ANALYSIS_BACKEND}/total-widget-taps?brand_id=${brandId}`
-    );
-    if (!response.ok) {
-      throw new Error("Error fetching total widget taps data");
-    }
-    return response.json();
-  }
-  
+  );
+  return response.data;
+}
--- src/app/brand/analytics/page.tsx
+++ src/app/brand/analytics/page.tsx
@@ -5,122 +5,28 @@ import {
   fetchTotalSentMessagesData,
   fetchTotalWidgetTapsData,
   fetchUniqueViewData,
+  getAverage,
+  getProducts,
+  getProductTaps,
 } from "@/api/getBrandAnalysis";
 import useBrandStore from "@/store/useBrandStore";
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { LuRefreshCw } from "react-icons/lu";
 import { MdDateRange } from "react-icons/md";
-
-const cardsData = [
-  {
-    title: "Unique views",
-    no: 155,
-    description: "Unique visitors who interacted with your AI chatbot.",
-  },
-  {
-    title: "Number of taps",
-    no: 250,
-    description: "Total user interactions with the chatbot.",
-  },
-  {
-    title: "Conversations started",
-    no: 155,
-    description: "Users who initiated a chat with your AI.",
-  },
-  {
-    title: "Number of Messages",
-    no: `2.2K`,
-    description: "Total messages exchanged between users and your bot.",
-  },
-  {
-    title: "Average message per conversation",
-    no: 75,
-    description: "The average number of messages per chat.",
-  },
-];
-
-const productsData = [
-  {
-    title: "Product 1",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 1",
-    taps: 155,
-  },
-  {
-    title: "Product 2",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 2",
-    taps: 250,
-  },
-  {
-    title: "Product 3",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 3",
-    taps: 155,
-  },
-  {
-    title: "Product 4",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 4",
-    taps: 220,
-  },
-  {
-    title: "Product 5",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 5",
-    taps: 180,
-  },
-  {
-    title: "Product 6",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 6",
-    taps: 300,
-  },
-  {
-    title: "Product 7",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 7",
-    taps: 120,
-  },
-  {
-    title: "Product 8",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 8",
-    taps: 210,
-  },
-  {
-    title: "Product 9",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 9",
-    taps: 270,
-  },
-  {
-    title: "Product 10",
-    image:
-      "https://img3.hkrtcdn.com/27426/prd_2742542-MuscleBlaze-Biozyme-Performance-Whey-4.4-lb-Rich-Chocolate_o.jpg",
-    description: "This is the description for Product 10",
-    taps: 400,
-  },
-];
+import { formatDistance } from "date-fns";
+import Counter from "@/component/Counter";
 
 const Page = () => {
-  const sortedProducts = productsData.sort((a, b) => b.taps - a.taps);
   const brandId = useBrandStore((state) => state.brandId);
+  const [refreshing, setRefreshing] = useState(false);
+  const [lastRefresh, setLastRefresh] = useState<Date | string | null>(null);
 
   const {
     data: uniqueViewData,
     isLoading: isLoadingUniqueView,
     error: errorUniqueView,
+    refetch: uniqueViewDataRefetch,
   } = useQuery({
     queryKey: ["uniqueView", brandId],
     queryFn: () => fetchUniqueViewData(brandId),
@@ -131,6 +37,7 @@ const Page = () => {
     data: totalSentMessagesData,
     isLoading: isLoadingSentMessages,
     error: errorSentMessages,
+    refetch: totalSentMessageDataRefetch,
   } = useQuery({
     queryKey: ["totalSentMessages", brandId],
     queryFn: () => fetchTotalSentMessagesData(brandId),
@@ -141,6 +48,7 @@ const Page = () => {
     data: totalFirstMessagesData,
     isLoading: isLoadingFirstMessages,
     error: errorFirstMessages,
+    refetch: totalFirstMessageDataRefetch,
   } = useQuery({
     queryKey: ["totalFirstMessages", brandId],
     queryFn: () => fetchTotalFirstMessagesData(brandId),
@@ -151,16 +59,106 @@ const Page = () => {
     data: totalWidgetTapsData,
     isLoading: isLoadingWidgetTaps,
     error: errorWidgetTaps,
+    refetch: totalWidgetTapDataRefetch,
   } = useQuery({
     queryKey: ["totalWidgetTaps", brandId],
     queryFn: () => fetchTotalWidgetTapsData(brandId),
     enabled: !!brandId,
   });
 
-  console.log("uniqueViewData", uniqueViewData);
-  console.log("totalSentMessagesData", totalSentMessagesData);
-  console.log("totalFirstMessagesData", totalFirstMessagesData);
-  console.log("totalWidgetTapsData", totalWidgetTapsData);
+  const {
+    data: averageData,
+    isLoading: isLoadingAverageData,
+    error: errorAverageData,
+    refetch: averageDataRefetch,
+  } = useQuery({
+    queryKey: ["averageData", brandId],
+    queryFn: () => getAverage(brandId),
+    enabled: !!brandId,
+  });
+
+  const {
+    data: productTitlesWithTaps,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["productTaps", brandId],
+    queryFn: () => getProductTaps(brandId),
+    enabled: !!brandId,
+  });
+
+  const {
+    data: products,
+    isLoading: productsLoading,
+    error: productError,
+  } = useQuery({
+    queryKey: ["products", brandId],
+    queryFn: () => getProducts(brandId, productTitlesWithTaps),
+    enabled: !!productTitlesWithTaps,
+  });
+
+  const cardsData = useMemo(
+    () => [
+      {
+        title: "Unique views",
+        no: errorUniqueView ? 0 : uniqueViewData || 0,
+        description: "Unique visitors who interacted with your AI chatbot.",
+      },
+      {
+        title: "Number of taps",
+        no: errorWidgetTaps ? 0 : totalWidgetTapsData || 0,
+        description: "Total user interactions with the chatbot.",
+      },
+      {
+        title: "Conversations started",
+        no: errorFirstMessages ? 0 : totalFirstMessagesData || 0,
+        description: "Users who initiated a chat with your AI.",
+      },
+      {
+        title: "Number of Messages",
+        no: errorSentMessages ? 0 : totalSentMessagesData || 0,
+        description: "Total messages exchanged between users and your bot.",
+      },
+      {
+        title: "Average message per conversation",
+        no: errorAverageData ? 0 : averageData || 0,
+        description: "The average number of messages per chat.",
+      },
+    ],
+    [
+      uniqueViewData,
+      totalWidgetTapsData,
+      totalFirstMessagesData,
+      totalSentMessagesData,
+      averageData,
+      brandId,
+    ]
+  );
+
+  console.log(isLoadingUniqueView, "123123");
+
+  async function handleRefresh() {
+    setRefreshing(true);
+    await Promise.all([
+      uniqueViewDataRefetch(),
+      totalSentMessageDataRefetch(),
+      totalFirstMessageDataRefetch(),
+      totalWidgetTapDataRefetch(),
+      averageDataRefetch(),
+    ]);
+    const now = new Date();
+    setLastRefresh("now");
+    localStorage.setItem(`last-refresh-${brandId}`, now.toISOString());
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    if (!brandId) return;
+    const storedTime = localStorage.getItem(`last-refresh-${brandId}`);
+    if (storedTime) {
+      setLastRefresh(new Date(storedTime));
+    }
+  }, [brandId]);
 
   return (
     <div className="ml-20 px-5  h-screen ">
@@ -168,10 +166,21 @@ const Page = () => {
         <h1 className="text-2xl font-bold py-5 sticky top-0  ">Analytics</h1>
         <div className="flex gap-5 items-center">
           <div className="flex gap-2 items-center text-base">
-            Updated just now{" "}
-            <div className="bg-[#1C1C1D] p-4 rounded-xl border-[1px]">
-              <LuRefreshCw />
-            </div>
+            {lastRefresh === "now"
+              ? "now"
+              : lastRefresh
+              ? formatDistance(lastRefresh, new Date(), { addSuffix: true })
+              : null}
+
+            <button
+              type="button"
+              className="bg-[#1C1C1D] p-4 rounded-xl border-[1px] cursor-pointer"
+              onClick={handleRefresh}
+            >
+              <LuRefreshCw
+                className={refreshing ? "animate-spin" : "animate-none"}
+              />
+            </button>
           </div>
           <div className="flex gap-2 items-center bg-[#1C1C1D] p-3 rounded-xl border-[1px]">
             date <MdDateRange />
@@ -185,7 +194,9 @@ const Page = () => {
             className="w-1/3 h-[180px] max-w-[calc(33.333%-1.25rem)] bg-[#161616] flex flex-col justify-between  p-4 rounded-xl shadow-md"
           >
             <h3 className="text-xl ">{card.title}</h3>
-            <p className="text-5xl font-medium mb-[-15px]">{card.no}</p>
+              <Counter endNumber={card.no} />
+              {/* <p className="text-5xl font-medium mb-[-15px]">{card.no}</p> */}
+
             <p className="text-sm">{card.description}</p>
           </div>
         ))}
@@ -197,35 +208,35 @@ const Page = () => {
           </div>
 
           {/* Product list */}
-          {sortedProducts.map((product) => (
+          {products?.map((product) => (
             <div
-              key={product.title}
+              key={product.product_name}
               className="w-full flex justify-between items-center p-4 rounded-xl shadow-md"
             >
               {/* Image and details on left */}
               <div className="flex">
                 <div className="flex items-center justify-center w-[78px] h-[73px] px-2 py-3 bg-[#2d2d2d] rounded-xl">
                   <img
-                    src={product.image}
-                    alt={product.title}
+                    src={product.product_images[0]}
+                    alt={product.product_name}
                     className=" object-cover rounded-xl"
                   />
                 </div>
                 <div className="ml-4 flex flex-col mt-2 gap-3">
                   {/* Title */}
                   <h3 className="text-sm font-semibold text-white">
-                    {product.title}
+                    {product.product_name}
                   </h3>
                   {/* Description */}
                   <p className="text-sm text-[#A4a4a4]">
-                    {product.description}
+                    {product.product_description}
                   </p>
                 </div>
               </div>
 
               {/* Number of taps on the right */}
               <div className="flex items-baseline gap-2 justify-center text-2xl font-medium text-white">
-                {product.taps} <span className="text-sm">taps</span>
+                {productTitlesWithTaps[product.product_name]}{" "}
               </div>
             </div>
           ))}
